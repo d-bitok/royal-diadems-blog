b311dd73bac2b644ce79c2a6a7bb64141b0dd839
rexdougie <bitokdouglas@gmail.com>
2021-12-04 16:52:15 +0000

    Linked to Home Page

--- components/Author.jsx
+++ components/Author.jsx
@@ -5,7 +5,7 @@ import { graphCMSImageLoader } from "../util";
 
 export default function Author({ author }) {
   return (
-    <div className="text-center mt-20 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="text-center mt-20 p-12 relative rounded-lg bg-white bg-opacity-20">
       <div className="absolute left-0 right-0 -top-14">
         <Image
           unoptimized
--- components/Header.jsx
+++ components/Header.jsx
@@ -15,16 +15,17 @@ export default function Header() {
     <div className="container mx-auto mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              Royal Diadems
-            </span>
-          </Link>
+          <span className="transition duration-700 cursor-pointer text-black hover:text-green-600 text-xl font-semibold">
+            <Link href="https://royal-diadems.web.app/"> Home </Link>
+          </span>
+          <span className="transition duration-700 cursor-pointer hover:text-green-600 text-black text-xl ml-6 font-semibold">
+            <Link href="/"> All </Link>
+          </span>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
